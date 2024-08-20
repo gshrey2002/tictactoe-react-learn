fb1e5ae971d5a72511ba2ff405e09bdaba14b6f4
Shrey <guptashrey163@gmail.com>
2024-08-20 17:48:06 +0530

    tic tac toe using FE react and html

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TicTacToe from "./component/assets/tic-tac-toe/TicTacToe"
+
+
+function App(){
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  )
+}
+    
+  
+
+
+export default App
